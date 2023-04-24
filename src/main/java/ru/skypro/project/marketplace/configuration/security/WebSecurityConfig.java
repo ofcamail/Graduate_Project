802e79de6ff7e9badf06285855861a465eba990b
ofcmail <ofcmailkom@gmail.com>
2023-04-24 16:42:51 +0300

    Last Commit

--- src/main/java/ru/skypro/project/marketplace/configuration/security/WebSecurityConfig.java
+++ src/main/java/ru/skypro/project/marketplace/configuration/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends GlobalMethodSecurityConfiguration {
